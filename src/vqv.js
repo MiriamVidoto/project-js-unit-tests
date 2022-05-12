269ca2b08edddc567187f75793e2def7a7e83112
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-05-12 17:00:23 -0300

    requisito 3 ok

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if (name === undefined || age === undefined) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
 
 module.exports = vqv;
